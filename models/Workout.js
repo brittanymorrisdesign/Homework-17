1ad7fb1e9e0d07fd73a6a2f64b0cdfe414a3069d
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-03-24 22:50:09 -0400

    updated schema

--- models/Workout.js
+++ models/Workout.js
@@ -1,1 +1,22 @@
-const mongoose = require('moongoose');
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const workoutSchema = new Schema({
+  day: { type: Date, default: Date.now },
+  exercises: [
+    {
+      type: { type: String, required: true },
+      name: { type: String },
+      duration: { type: Number },
+      weight: { type: Number },
+      reps: { type: Number },
+      sets: { type: Number },
+      distance: { type: Number },
+    },
+  ],
+});
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
