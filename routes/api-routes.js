e174f73e8fbcd1978590ef0d22f2cf5acd970162
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-03-26 12:39:00 -0400

    updated api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,14 @@
 // Dependencies
 // =============================================================
 const router = require('express').Router();
-const Workout = require('../models/');
+const db = require('../models/');
 
 // Routes
 // =============================================================
 
 // Gets all previous workouts
 router.get('/api/workouts', (req, res) => {
-  Workout.find({})
+  db.Workout.find({})
     .then(dbWorkout => {
       console.log(dbWorkout);
       res.json(dbWorkout);
@@ -20,7 +20,7 @@ router.get('/api/workouts', (req, res) => {
 
 // Add user's new workout
 router.post('/api/workouts', ({ body }, res) => {
-  Workout.create(body)
+  db.Workout.create(body)
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -31,7 +31,10 @@ router.post('/api/workouts', ({ body }, res) => {
 
 // PUT route for updating workout exercises by id
 router.put('/api/workouts/:id', (req, res) => {
-  Workout.updateOne({ _id: req.params.id }, { $push: { exercises: req.body } })
+  db.Workout.updateOne(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } }
+  )
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -42,7 +45,7 @@ router.put('/api/workouts/:id', (req, res) => {
 // Get request for last 7 workouts
 router.get('/api/workouts/range', (req, res) => {
   // Organizes workouts by most recent first
-  Workout.find({})
+  db.Workout.find({})
     .sort({ _id: -1 })
     .limit(7)
     .then(dbWorkout => {
